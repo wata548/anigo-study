3a2dc3631f8576259b8c1bff51439cfc16a50103
skywind99 <skywind99@naver.com>
2025-12-15 16:22:56 +0900

    Update DashboardView.tsx

--- src/components/DashboardView.tsx
+++ src/components/DashboardView.tsx
@@ -252,7 +252,7 @@ const DashboardView: React.FC<DashboardViewProps> = ({
           좌석 배치도
         </h3>
         <img
-          src="https://raw.githubusercontent.com/skywind99/anigo-study/refs/heads/main/src/position.jpg"
+          src="https://drive.google.com/file/d/16q-xGj9hr_D06w0SKb9JJJ4CpZ5nrSeq/view?usp=sharing"
           alt="좌석 배치도"
           style={{
             width: "100%",
@@ -518,6 +518,7 @@ const DashboardView: React.FC<DashboardViewProps> = ({
 };
 
 export default DashboardView;
+
 
 
 
