5bd8517aea7cad65ccea1d85cb16560e2a29b03d
wata548 <watashimokia@gmail.com>
2026-02-03 11:27:35 +0900

    link sucess

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { Console } from "console";
 
-const supabaseUrl = process.env.DB_URL as string;
-const OIDCToken = process.env.VERCEL_OIDC_TOKEN as string;
-console.log(supabaseUrl);
-console.log(OIDCToken);
+const supabaseUrl = process.env.REACT_APP_DB_URL as string;
+const OIDCToken = process.env.REACT_APP_VERCEL_OIDC_TOKEN as string;
 export const supabase = createClient(supabaseUrl, OIDCToken);
